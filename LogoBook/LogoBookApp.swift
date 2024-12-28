8f7f08a9e43386b438617f4d62d48e028b6ae8f2
Keisuke2162 <applabo.kei@gmail.com>
2024-12-29 07:16:30 +0900

    マルチモジュール化

--- LogoBook/LogoBookApp.swift
+++ LogoBook/LogoBookApp.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import SwiftData
+import Features
 
 @main
 struct LogoBookApp: App {
---
+++ LogoBookPackage/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LogoBookPackage",
+    platforms: [.iOS(.v17)],
+    products: [
+      .library(name: "Features", targets: ["Features"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/firebase/firebase-ios-sdk", exact: "11.6.0"),
+      .package(url: "https://github.com/google/GoogleSignIn-iOS", exact: "8.0.0"),
+      .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "8.1.2"),
+    ],
+    targets: [
+      .target(name: "Entities"),
+      .target(name: "Extensions"),
+      .target(name: "Features"),
+      .target(name: "Repositories"),
+      .target(name: "UseCases"),
+    ]
+)
