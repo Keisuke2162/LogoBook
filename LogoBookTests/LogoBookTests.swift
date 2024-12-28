d13099a0a4f28cc1cbfff8e0acce077ee76b0986
Keisuke2162 <applabo.kei@gmail.com>
2024-12-29 06:30:09 +0900

    Initial Commit

---
+++ LogoBookTests/LogoBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogoBookTests.swift
+//  LogoBookTests
+//
+//  Created by Kei on 2024/12/29.
+//
+
+import Testing
+@testable import LogoBook
+
+struct LogoBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
